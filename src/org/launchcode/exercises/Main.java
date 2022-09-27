e4c180fa9c483481f902c5d1ea2a87cdfaf4172d
leerohrer1 <loginlee3@gmail.com>
2022-09-27 12:54:45 -0500

    Finished exercises

--- src/org/launchcode/exercises/Main.java
+++ src/org/launchcode/exercises/Main.java
@@ -7,7 +7,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // Test out your Divide() function!
+        Divide(0, 8);
+        Divide(2, 0);
 
         HashMap<String, String> studentFiles = new HashMap<>();
         studentFiles.put("Carl", "Program.java");
@@ -15,17 +16,41 @@ public class Main {
         studentFiles.put("Elizabeth", "MyCode.java");
         studentFiles.put("Stefanie", "CoolProgram.java");
 
-        // Test out your CheckFileExtension() function!
+        for (Map.Entry<String, String> student : studentFiles.entrySet()) {
+            System.out.println(student.getKey());
+            CheckFileExtension(student.getValue());
+        }
+
     }
 
     public static void Divide(int x, int y)
     {
-        // Write code here!
+        if(y == 0) {
+            try {
+                throw new ArithmeticException("You can't divide by 0.");
+            } catch (ArithmeticException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public static int CheckFileExtension(String fileName)
     {
-        // Write code here!
+        int points = 0;
+        if(fileName.endsWith(".java")) {
+            points = 1;
+        } else if(fileName.equals("") || fileName.equals(null)) {
+            points = -1;
+            try {
+                throw new Exception("That's not a valid file name.");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            points = 0;
+        }
+
+        return points;
     }
 
 }
